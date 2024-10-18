d2d8047fa68ee575aa2eb5a46ec5a49f0f7892da
Sytsma, Menno <menno.sytsma@wur.nl>
2024-10-18 09:56:58 +0200

    changes in offline image publisher

--- follow_the_leader/follow_the_leader/offline_image_publisher.py
+++ follow_the_leader/follow_the_leader/offline_image_publisher.py
@@ -7,7 +7,7 @@ from rclpy.node import Node
 from follow_the_leader.networks.flowgan import FlowGAN
 from cv_bridge import CvBridge
 import tf2_ros
-from scipy.spatial.transform import Rotation
+from scipy.spatial.transform import Rotation as R
 from sensor_msgs.msg import Image, CameraInfo
 from geometry_msgs.msg import TransformStamped
 import time
@@ -38,14 +38,20 @@ def read_trajectory(filename):
     return traj
 
 class ImagePublisherNode(Node):
-    def __init__(self, image_folder):
+    def __init__(self):
         super().__init__('image_publisher_node')
-        self.image_folder = image_folder
+        # self.image_folder = image_folder    
+        self.image_folder = "/home/ubuntu/FTL/data/color"
+        trajectory_file = "/home/ubuntu/FTL/data/trajectory.log"
+        self.camera_poses = read_trajectory(trajectory_file) 
+        self.image_files = sorted([filename for filename in os.listdir(self.image_folder)])  # Sort the files of the color frames to process sequentially
+        self.max_publish_count = len(self.image_files)
+        self.publish_count = 0
         size=(848, 480)
         self.tf_broadcaster = tf2_ros.TransformBroadcaster(self)
-        self.image_publisher = self.create_publisher(Image, '/camera/color/image_raw', 10)
-        self.cam_info_pub = self.create_publisher(CameraInfo, "/camera/color/camera_info", 1)
-
+        self.image_publisher = self.create_publisher(Image, '/camera/color/image_rect_raw', 10)
+        self.image_pub_raw = self.create_publisher(Image, '/camera/color/image_raw', 10)
+        self.cam_info_pub = self.create_publisher(CameraInfo, "/camera/color/camera_info", 2)
         # Setup camera
         
         self.cam_info = CameraInfo(
@@ -59,29 +65,57 @@ class ImagePublisherNode(Node):
             k=[437.00222778, 0.0, 418.9420166, 0.0, 439.22055054, 240.41038513,0.0,0.0,1.0],
             p=[437.00222778, 0.0, 418.9420166, 0.0, 0.0, 439.22055054, 240.41038513, 0.0, 0.0, 0.0, 1.0, 0.0])
         self.cam_info.header.frame_id = "camera_color_optical_frame"
+        self.cam_info.header.stamp = self.get_clock().now().to_msg()
+        timer_period = 0.15
+        self.timer = self.create_timer(timer_period,self.timer_callback)
+
+    def timer_callback(self):
+        # If we've reached the maximum count, shut down the node
+        if self.publish_count >= self.max_publish_count:
+            self.get_logger().info('Reached maximum publish count, shutting down...')
+            raise SystemExit
+
+        self.publish_images(self.image_files[self.publish_count],self.camera_poses[self.publish_count].pose)
+
+        # Increment the publish count
+        self.publish_count += 1
 
 
     def publish_pose(self, camera_pose):
         transform = TransformStamped()
         transform.header.stamp = self.get_clock().now().to_msg()
         transform.header.frame_id = 'base_link'
-        transform.child_frame_id = 'tool0'
-        transform.transform.translation.x = camera_pose[0, 3]
-        transform.transform.translation.y = camera_pose[2, 3]
-        transform.transform.translation.z = camera_pose[1, 3]*-1
-        quat = Rotation.from_matrix(camera_pose[:3, :3]).as_quat()
-        angles_deg = [0, 180, 0]  # Roll, Pitch, Yaw
-        r = Rotation.from_euler('zyx', angles_deg, degrees=True)
-        quat = Rotation.__mul__(r,Rotation.from_matrix(camera_pose[:3, :3])).as_quat()
+        transform.child_frame_id = 'camera_color_optical_frame'
+        pos_cam_base = camera_pose[:3,3]
+        # Define the 90-degree rotation around the Y-axis for the base frame
+        base_rotation = R.from_euler('xyz', [-90,0,-90], degrees=True)  # Rotation object
+        # Apply the rotation to the translation vector
+        transformed_pos = base_rotation.apply(pos_cam_base)
+
+        transform.transform.translation.x = transformed_pos[0]
+        transform.transform.translation.y = transformed_pos[1]
+        transform.transform.translation.z = transformed_pos[2]
+        # quat = R.from_matrix(camera_pose[:3, :3]).as_quat()
+        quat = R.__mul__(base_rotation,R.from_matrix(camera_pose[:3, :3])).as_quat()
+        transform.transform.rotation.x = quat[0]
+        transform.transform.rotation.y = quat[1]
+        transform.transform.rotation.z = quat[2]
+        transform.transform.rotation.w = quat[3]
+        self.tf_broadcaster.sendTransform(transform)
+        # quat = R.from_matrix(camera_pose[:3, :3]).as_quat()
+        angles_deg = [90, 0, 90]  # Roll, Pitch, Yaw
+        r = R.from_euler('zyx', angles_deg, degrees=True)
+        # quat = R.__mul__(r,R.from_matrix(camera_pose[:3, :3])).as_quat()
+        quat = R.__mul__(R.__mul__(base_rotation,R.from_matrix(camera_pose[:3, :3])),r).as_quat()
+        # quat = R.from_matrix(camera_pose[:3, :3]).as_quat()
         # if np.linalg.norm(quat) > np.radians(0.5):
         #     print("ROTATION TOO BIGGGG")
         transform.transform.rotation.x = quat[0]
         transform.transform.rotation.y = quat[1]
         transform.transform.rotation.z = quat[2]
         transform.transform.rotation.w = quat[3]
+        transform.child_frame_id = 'tool0'
         self.tf_broadcaster.sendTransform(transform)
-        # transform.child_frame_id = 'camera_color_optical_frame'
-        # self.tf_broadcaster.sendTransform(transform)
 
     def publish_images(self,img_name, camera_pose):        
         init_image_path = os.path.join(self.image_folder, img_name)
@@ -99,22 +133,29 @@ class ImagePublisherNode(Node):
         # result = cv2.bitwise_and(result, mask)
         resized_image = cv2.resize(img, (424, 240))
         msg = bridge.cv2_to_imgmsg(resized_image, encoding="rgb8")
-        self.image_publisher.publish(msg)        
+        # Add current time to the header
+        msg.header.stamp = self.get_clock().now().to_msg()
+
+        # Set the frame ID to "camera_color_optical_frame"
+        msg.header.frame_id = "camera_color_optical_frame"
+        self.image_publisher.publish(msg)   
+        self.image_pub_raw.publish(msg)
+        self.cam_info.header.stamp = self.get_clock().now().to_msg()        
         self.cam_info_pub.publish(self.cam_info)
         self.publish_pose(camera_pose)
-        time.sleep(0.3)
 
 def main(args=None):
     rclpy.init(args=args)
-    image_folder = "/home/ubuntu/FTL/data/color"
-    trajectory_file = "/home/ubuntu/FTL/data/trajectory.log"
-    camera_poses = read_trajectory(trajectory_file) 
 
-    node = ImagePublisherNode(image_folder)
-    image_files = sorted([filename for filename in os.listdir(image_folder)])  # Sort the files of the color frames to process sequentially
-    for i in range(len(image_files)-1):
-        node.publish_images(image_files[i],camera_poses[i].pose)
-    rclpy.spin(node)
+
+    node = ImagePublisherNode()
+    # for i in range(len(image_files)-1):
+    #     node.publish_images(image_files[i],camera_poses[i].pose)
+    try:
+        rclpy.spin(node)
+    except SystemExit: # <-- process the exception
+        rclpy.logging.get_logger("Quitting").info('Done')
+    
     node.destroy_node()
     rclpy.shutdown()
 
